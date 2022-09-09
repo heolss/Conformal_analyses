ecf22328e53040e3e849b52a28429c42c94d3904
heolss <henrikolsson_84@hotmail.com>
2022-09-09 14:29:54 +0200

    Updating/simplify folder structure for analysis scripts

--- master.R
+++ master.R
@@ -5,49 +5,49 @@ rm(list=ls())
 ## Main scripts
 ########################################################################################
 # 1. Datasources - Table1
-source("datasources.R")
+source("analysis/datasources.R")
 datasources
 
 # 2. Results - Test-set
-source("CP_LO_argmaxvoting/CP_LO_TEST.R")
+source("analysis/CP_LO_TEST.R")
 tab01;tab20;tab33
 grid.arrange(cx, isup, ncol = 2)
 
 # 3. Results - Imagebase
-source("CP_LO_argmaxvoting/CP_LO_IMAGEBASE.R")
+source("analysis/CP_LO_IMAGEBASE.R")
 tab01;tab20;tab33
 grid.arrange(cx, isup, ncol = 2)
 
 # 4. Results - KS
-source("CP_LO_argmaxvoting/CP_LO_KS.R")
+source("analysis/CP_LO_KS.R")
 tab01;tab20;tab33
 grid.arrange(cx, isup, ncol = 2)
 
 # 5. Results - CP on Rare prostate tissue morphology by subtype
-source("rare_subtypes/CP_rare_subtypes_KS_by_subtype.R")
+source("analysis/CP_rare_subtypes_KS_by_subtype.R")
 tab01;tab05;tab1
 cx
 
 # 6. Results - AI without CP on Rare prostate tissue morphology by subtype
-source("rare_subtypes/AI_rare_subtypes_KS_by_subtypes.R")
+source("analysis/AI_rare_subtypes_KS_by_subtypes.R")
 AI_CX_subtype
 
 # 8. Results - STHLM3-Aperio model evaluted on 448 paired slides (scanned on both Aperio & Hamamatsu)
-source("STHLM3_APERIO/paired_aperio.R")
+source("analysis/paired_aperio.R")
 tab01;tab20;tab33
 grid.arrange(cx, isup, ncol = 2)
-source("STHLM3_APERIO/paired_hamamatsu.R")
+source("analysis/paired_hamamatsu.R")
 tab01;tab20;tab33
 grid.arrange(cx, isup, ncol = 2)
 
 # 9. Kolmogorov-Smirnov test of equality of the distribution of the predictions in the calibration set and 
 # each test dataset to test the validity of the prediction regions. 
 # The null hypothesis was that the samples are drawn from the same distribution.
-source("CP_LO_argmaxvoting/kolmogorov_pvalues.R")
+source("analysis/kolmogorov_pvalues.R")
 p_values
 
 # 10. Prediction-regions by Imagebase-ISUP-mode and coverage by individual Imagebase-pathologist-votes
-source("CP_LO_argmaxvoting/imagebase_voting_by_predgroups.R")
+source("analysis/imagebase_voting_by_predgroups.R")
 t1; t2; summary(df$Frequency)
 
 ################################## end of program #################################
